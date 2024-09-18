28f47a2087284ddd51a6f855fd825101f6a7918b
hc-syn <hugo.clout@synacktiv.com>
2024-09-18 14:14:48 +0200

    make listener config dynamic

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"io"
+	"maps"
 	"os"
 	"os/signal"
+	"slices"
 	"syscall"
 	"time"
 
@@ -12,6 +14,7 @@ import (
 
 var gChainsConf chainsConf
 var gRoutingConf routingConf
+var gServerConf serverConf
 var gHosts map[string]string
 var gMetaLogger *logger.MetaLogger
 
@@ -80,21 +83,6 @@ func main() {
 
 	// ***** END Logs setup *****
 
-	// ***** BEGIN Server args parsing *****
-
-	var servers []server
-	for _, s := range gArgServers {
-		server, err := newServer(s)
-		if err != nil {
-			gMetaLogger.Errorf("could not create new server with string %s: %v", s, err)
-			continue
-		}
-		servers = append(servers, *server)
-		gMetaLogger.Debugf("added server %s to servers list", server.String())
-	}
-
-	// ***** END Server args parsing ******
-
 	// ***** BEGIN Configuration files loading *****
 
 	// Output PID needed to hot reload configuration files
@@ -105,102 +93,162 @@ func main() {
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGHUP)
 
-	// Wait for data on the previously created channel to reload configuration files
-	go func() {
-		for {
-			sig := <-signalCh
-			gMetaLogger.Infof("Signal %v received, reloading configurations", sig)
-
-			// If -pac is not defined, use JSON routing configuration file
-			if gArgPACPath == "" {
-				routing, err := parseRoutingConfig(gArgRoutingConfigPath)
-				if err != nil {
-					gMetaLogger.Errorf("error parsing JSON routing configuration: %v", err)
-					continue
-				}
-				var allExists bool
-				allExists = true
-
-				for _, s := range servers {
-					_, exists := routing[s.table]
-					if !exists {
-						gMetaLogger.Errorf("routing table %s is not defined in the parsed routing configuration", s.table)
-						allExists = false
-					}
-				}
-
-				if !allExists {
-					continue
-				}
-
-				gRoutingConf.mu.Lock()
-				gRoutingConf.routing = routing
-				gRoutingConf.valid = true
-				gRoutingConf.mu.Unlock()
-				gMetaLogger.Info("Global JSON routing configuration updated")
-				gMetaLogger.Debugf("routing config: %v", routing)
-			} else { // Otherwise, use PAC file
-				err := reloadPACConf(gArgPACPath)
-				if err != nil {
-					gMetaLogger.Errorf("error reloading pac file: %v", err)
-					continue
-				}
-				gMetaLogger.Info("Global PAC configuration updated")
-			}
-
-			// Load proxies and chains configuration
-			confProxyChains, err := parseConfig(gArgConfigPath)
-			if err != nil {
-				gMetaLogger.Errorf("error parsing JSON configuration: %v", err)
-				continue
-			}
-
-			gChainsConf.mu.Lock()
-			gChainsConf.proxychains = confProxyChains
-			gChainsConf.valid = true
-			gChainsConf.mu.Unlock()
-			gMetaLogger.Info("Global JSON configuration updated")
-
-			// Load custom host resolution config file
-			if gArgCustomHosts != "" {
-				var tmpHosts map[string]string
-				tmpHosts, err = loadHosts(gArgCustomHosts)
-				if err != nil {
-					gMetaLogger.Errorf("error parsing custom hosts file: %v", err)
-					continue
-				}
-
-				gHosts = tmpHosts
-				gMetaLogger.Info("Global host resolution configuration updated")
-			}
-
-		}
-	}()
-
 	// Send a SIGHUP to trigger initial configuration loading
 	signalCh <- syscall.SIGHUP
 
-	// Wait for a first valid configuration to be loaded
-	gMetaLogger.Info("Waiting for valid configuration file")
-	valid := false
-	for !valid {
-		gChainsConf.mu.RLock()
-		valid = gChainsConf.valid
-		gChainsConf.mu.RUnlock()
-		time.Sleep(1 * time.Second)
-	}
-	gMetaLogger.Info("Valid configuration loaded, running server(s)")
-
-	// ***** END Configuration files loading *****
-
-	// ***** BEGIN Run server *****
-
-	for _, s := range servers {
-		go s.run()
-	}
-
-	// ***** END Run server *****
+	// Wait for data on the previously created channel to reload configuration files
 	for {
-		time.Sleep(1 * time.Hour)
+		sig := <-signalCh
+		gMetaLogger.Infof("Signal %v received, reloading configurations", sig)
+
+		var servers []server
+		var routing routing
+
+		gMetaLogger.Debug("Describing gServerConf.servers : ")
+		describeServers(gServerConf.servers)
+
+		// Load proxies and chains configuration
+		confProxyChains, err := parseConfig(gArgConfigPath)
+		if err != nil {
+			gMetaLogger.Errorf("error parsing JSON configuration: %v", err)
+			continue
+		}
+
+		// If -pac is not defined, load JSON routing configuration file, load servers and perform consistency checks
+		if gArgPACPath == "" {
+			routing, err = parseRoutingConfig(gArgRoutingConfigPath)
+			if err != nil {
+				gMetaLogger.Errorf("error parsing JSON routing configuration: %v", err)
+				continue
+			}
+
+			// Check that all routes defined in routing correspond to an existing chain in confProxyChains
+			allExist := true
+			definedRoutes := slices.Collect(maps.Keys(confProxyChains))
+			for _, routingTable := range routing {
+				for _, ruleBlock := range routingTable {
+
+					if !slices.Contains(definedRoutes, ruleBlock.Route) {
+						gMetaLogger.Errorf("route %v defined in routing configuration is not defined in the chain configuration (%v)", ruleBlock.Route, definedRoutes)
+						allExist = false
+					}
+				}
+			}
+
+			if !allExist {
+				continue
+			}
+
+			// Load server configuration
+			servers, err = parseServerConfig("server.json")
+			if err != nil {
+				gMetaLogger.Errorf("error parsing JSON server configuration: %v", err)
+				continue
+			}
+
+			// Check that all servers defined use an existing routing table
+			allExist = true
+			definedTables := slices.Collect(maps.Keys(routing))
+			for _, server := range servers {
+				if !slices.Contains(definedTables, server.table) {
+					gMetaLogger.Errorf("table %v defined in server configuration is not defined in the routing configuration (%v)", server.table, definedTables)
+					allExist = false
+				}
+			}
+
+			if !allExist {
+				continue
+			}
+
+		} else { // Otherwise, load PAC file, load servers,  and do not perform consistency checks
+			err := reloadPACConf(gArgPACPath)
+			if err != nil {
+				gMetaLogger.Errorf("error reloading pac file: %v", err)
+				continue
+			}
+			gMetaLogger.Info("Global PAC configuration updated")
+
+			// Load server configuration
+			servers, err = parseServerConfig("server.json")
+			if err != nil {
+				gMetaLogger.Errorf("error parsing JSON server configuration: %v", err)
+				continue
+			}
+		}
+
+		// Load custom host resolution config file
+		if gArgCustomHosts != "" {
+			var tmpHosts map[string]string
+			tmpHosts, err = loadHosts(gArgCustomHosts)
+			if err != nil {
+				gMetaLogger.Errorf("error parsing custom hosts file: %v", err)
+				continue
+			}
+			gHosts = tmpHosts
+			gMetaLogger.Info("Global host resolution configuration updated")
+		}
+
+		// At this point, the defined configuration should be consistent, so we can update the globals
+
+		// Update global variables if all the configuration files are consistent
+		gChainsConf.mu.Lock()
+		gChainsConf.proxychains = confProxyChains
+		gChainsConf.valid = true
+		gChainsConf.mu.Unlock()
+		gMetaLogger.Info("Global JSON configuration updated")
+
+		if gArgPACPath == "" {
+			gRoutingConf.mu.Lock()
+			gRoutingConf.routing = routing
+			gRoutingConf.valid = true
+			gRoutingConf.mu.Unlock()
+			gMetaLogger.Info("Global JSON routing configuration updated")
+			gMetaLogger.Debugf("routing config: %v", routing)
+		}
+
+		// Update global servers variable, stop old ones and start new ones
+
+		// Stoping running servers that are not defined in the new configuration
+		gMetaLogger.Debug("Describing servers : ")
+		describeServers(servers)
+		gServerConf.mu.Lock()
+		for i := range gServerConf.servers {
+			stillExists := slices.ContainsFunc(servers, func(s server) bool { return compare(s, gServerConf.servers[i]) })
+			if stillExists {
+				gMetaLogger.Debugf("Server %v still exists in new loaded servers, keeping it", gServerConf.servers[i])
+			} else {
+				gMetaLogger.Debugf("Server %v does not exists anymore, stopping it", gServerConf.servers[i])
+				gServerConf.servers[i].stop()
+				gServerConf.servers = slices.Delete(gServerConf.servers, i, i+1)
+
+			}
+		}
+
+		for i := range servers {
+			alreadyExists := slices.ContainsFunc(gServerConf.servers, func(s server) bool { return compare(s, servers[i]) })
+			if !alreadyExists {
+				gServerConf.servers = append(gServerConf.servers, servers[i])
+			}
+		}
+
+		gServerConf.mu.Unlock()
+
+		gMetaLogger.Debugf("gServerConf.servers : %v", gServerConf.servers)
+		gMetaLogger.Debug("Describing gServerConf.servers : ")
+		describeServers(gServerConf.servers)
+
+		// Start all servers that are not running
+		for i := 0; i < len(gServerConf.servers); i++ {
+			if !gServerConf.servers[i].running {
+				gMetaLogger.Debugf("myServer %v(%p) is not running, running it", gServerConf.servers[i], &gServerConf.servers[i])
+				go (gServerConf.servers[i]).run()
+				time.Sleep(1 * time.Second)
+				gMetaLogger.Debugf("myServer %v(%p) is running", gServerConf.servers[i], &gServerConf.servers[i])
+			}
+		}
+
+		gMetaLogger.Debug("Describing gServerConf.servers : ")
+		describeServers(gServerConf.servers)
+
 	}
 }
--- server.go
+++ server.go
@@ -3,10 +3,13 @@ package main
 // Defines functions to run the input servers (SOCKS5 and HTTP CONNECT) and to handle incomming client connections.
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
+	"os"
 	"strings"
 	"sync"
 )
@@ -31,11 +34,49 @@ type server struct {
 	port    string
 	table   string
 	handler connHandler
+	ctx     context.Context
+	cancel  context.CancelFunc
+	running bool
 }
 
-func newServer(srvString string) (*server, error) {
+// serverConf is the type used to hold and access a server configuration (defined in a file)
+type serverConf struct {
+	servers []server
+	valid   bool // whether the current configuration is valid
+	mu      sync.RWMutex
+}
+
+func newServer(prot string, addr string, port string, table string) (*server, error) {
 	gMetaLogger.Debugf("Entering newServer()")
 	defer gMetaLogger.Debugf("Leaving newServer()")
+
+	var handler connHandler
+
+	switch prot {
+	case "socks5":
+		handler = new(socks5Handler)
+	case "http":
+		handler = new(httpHandler)
+	default:
+		return nil, fmt.Errorf("%v handler type does not exist", prot)
+	}
+
+	s := &server{
+		prot:    prot,
+		addr:    addr,
+		port:    port,
+		table:   table,
+		handler: handler,
+		ctx:     nil,
+		cancel:  nil,
+		running: false,
+	}
+	return s, nil
+}
+
+func newServerFromString(srvString string) (*server, error) {
+	gMetaLogger.Debugf("Entering newServerFromString()")
+	defer gMetaLogger.Debugf("Leaving newServerFromString()")
 
 	s1 := strings.Split(srvString, "://")
 	if len(s1) != 2 {
@@ -53,24 +94,66 @@ func newServer(srvString string) (*server, error) {
 	port := s3[1]
 	table := s3[2]
 
-	s := new(server)
+	return newServer(prot, addr, port, table)
+}
 
-	s.addr = addr
-	s.port = port
-	s.table = table
+// Custom JSON unmarshaller describing how to parse a server type from a string like "socsk5://127.0.0.1:1337:table1"
+func (server *server) UnmarshalJSON(b []byte) error {
 
-	switch prot {
-	case "socks5":
-		s.prot = prot
-		s.handler = new(socks5Handler)
-	case "http":
-		s.prot = prot
-		s.handler = new(httpHandler)
-	default:
-		return nil, fmt.Errorf("%v handler type does not exist", prot)
+	var serverString string
+
+	err := json.Unmarshal(b, &serverString)
+	if err != nil {
+		err = fmt.Errorf("error unmarshalling '%s' in string : %v", b, err)
+		return err
 	}
 
-	return s, nil
+	tmpServer, err := newServerFromString(serverString)
+	if err != nil {
+		err = fmt.Errorf("error creating new server from string: %v", err)
+		return err
+	}
+
+	server.addr = tmpServer.addr
+	server.port = tmpServer.port
+	server.prot = tmpServer.prot
+	server.table = tmpServer.table
+	server.ctx = tmpServer.ctx
+	server.cancel = tmpServer.cancel
+	server.handler = tmpServer.handler
+
+	return nil
+}
+
+// parseServerConfig parses the JSON servers configuration file at configPath and returns a []server variable representing this configuration.
+// The config file should be a JSON like :
+// [
+//
+//	"socks5://127.0.0.1:1080:table1",
+//	"http://127.0.0.1:8080:table2"
+//
+// ]
+// If PAC is used, the table is ignored and can be set to "pac"
+func parseServerConfig(configPath string) ([]server, error) {
+
+	fileBytes, err := os.ReadFile(configPath)
+	if err != nil {
+		err := fmt.Errorf("error reading file %v : %v", configPath, err)
+		return nil, err
+	}
+
+	var servers []server
+
+	dec := json.NewDecoder(bytes.NewReader(fileBytes))
+	dec.DisallowUnknownFields()
+
+	err = dec.Decode(&servers)
+	if err != nil {
+		err = fmt.Errorf("error unmarshalling server config file : %v", err)
+		return nil, err
+	}
+
+	return servers, nil
 }
 
 func (s server) address() string {
@@ -78,34 +161,71 @@ func (s server) address() string {
 }
 
 func (s server) String() string {
-	return fmt.Sprintf("%s://%s:%s:%s", s.prot, s.addr, s.port, s.table)
+	return fmt.Sprintf("%s://%s:%s:%s[running:%v, handler:%v]", s.prot, s.addr, s.port, s.table, s.running, s.handler)
 }
 
 // run runs an input server of type serverType listening on address
-func (s server) run() {
+func (s *server) run() {
+	gMetaLogger.Debugf("Entering %v(%p).run()", s, s)
+	defer gMetaLogger.Debugf("Leaving %v(%p).run()", s, s)
+
+	// Create a new context and store it in the server struct
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	s.ctx = ctx
+	s.cancel = cancel
+	s.running = true
 
 	// Creates a TCP socket and listen on address for incomming client connections
 	l, err := net.Listen("tcp", s.address())
 	if err != nil {
 		gMetaLogger.Panic(err)
 	}
+	defer l.Close()
 	gMetaLogger.Infof("connHandler started on %v", s.address())
 
 	// For each client connection received on the listening socket, create a context and start a goroutine handling the connection
 	for {
-		var c net.Conn
-		c, err = l.Accept()
-		if err != nil {
-			gMetaLogger.Error(err)
+		acceptDone := make(chan struct{})
+		go func() {
+			var c net.Conn
+			c, err = l.Accept()
+			if err != nil {
+				gMetaLogger.Error(err)
+				close(acceptDone)
+				return
+			}
+			gMetaLogger.Debugf("new connection (%v) accepted", c)
+
+			ctx, cancel := context.WithCancel(s.ctx)
+
+			go s.handler.connHandle(c, s.table, ctx, cancel)
+			close(acceptDone)
+		}()
+
+		select {
+		case <-s.ctx.Done():
+			return //causes l to be closed (see defer upper) and thus the last running Accept goroutine to return.
+		case <-acceptDone:
 			continue
 		}
-		gMetaLogger.Debugf("new connection (%v) accepted", c)
-
-		ctx, cancel := context.WithCancel(context.Background())
-
-		go s.handler.connHandle(c, s.table, ctx, cancel)
-
 	}
+}
+
+func (s *server) stop() {
+	gMetaLogger.Debugf("Entering %v.stop()", s)
+	defer gMetaLogger.Debugf("Leaving %v.stop()", s)
+
+	if s.running {
+		gMetaLogger.Debugf("%v server is running, stopping it.", s)
+		s.cancel()
+		s.running = false
+	}
+}
+
+func compare(s1 server, s2 server) (equal bool) {
+	equal = ((s1.addr == s2.addr) && (s1.port == s2.port) && (s1.prot == s2.prot) && (s1.table == s2.table))
+	return
 }
 
 // relay takes two net.Conn target and client (representing TCP sockets) and transfers data between them.
@@ -143,8 +263,15 @@ func relay(client net.Conn, target net.Conn) {
 		}
 	}()
 
-	gMetaLogger.Debug("Waiting for both goroutines to complete")
+	gMetaLogger.Debug("Waiting for both relay goroutines to complete")
 	wg.Wait()
-	gMetaLogger.Debug("Goroutines ended")
+	gMetaLogger.Debug("Relay goroutines ended")
 
+}
+
+func describeServers(servers []server) {
+	gMetaLogger.Debugf("Describing server slice %p : %v", servers, servers)
+	for i := 0; i < len(servers); i++ {
+		gMetaLogger.Debugf("Index %v. Server %p : %v", i, &(servers[i]), servers[i])
+	}
 }
